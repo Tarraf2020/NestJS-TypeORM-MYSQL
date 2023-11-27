6d818b6e1f0d4b578c5a801c4ad6ccabc977899d
Tarraf2020 <ali.tarraf2017@gmail.com>
2023-11-27 14:43:35 +0200

    typeORM

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
+import { CreateUserProfileDto } from '../../dtos/CreateUserProfile.dto';
+import { UsersService } from '../../services/users/users.service';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
+import { CreateUserPostDto } from './CreateUserPost.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Get()
+  getUsers() {
+    return this.userService.findUsers();
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  async updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    await this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
+  }
+
+  @Post(':id/profiles')
+  createUserProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() createUserProfileDto: CreateUserProfileDto,
+  ) {
+    return this.userService.createUserProfile(id, createUserProfileDto);
+  }
+
+  @Post(':id/posts')
+  createUserPost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() createUserPostDto: CreateUserPostDto,
+  ) {
+    return this.userService.createUserPost(id, createUserPostDto);
+  }
+}
